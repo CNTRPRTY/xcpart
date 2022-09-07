e16393d93b53da4c3b5144c7d5792a3e2aaa5775
Juan <dev@juanaleman.com>
2022-09-07 10:34:45 -0400

    latest headings adjustments

--- src/routes/latest.jsx
+++ src/routes/latest.jsx
@@ -39,7 +39,10 @@ export default class Latest extends React.Component {
             <main style={{ padding: "1rem" }}>
                 {/* <main style={{ padding: "1rem 0" }}> */}
 
-                <h1>Latest active addresses:</h1>
+                <h1>Latest:</h1>
+
+                <h2>Active addresses:</h2>
+                {/* <h1>Latest active addresses:</h1> */}
                 {this.state.addresses.length === 0 ? 'loading...' : null}
                 {this.state.addresses.map((address) => (
                     <ul key={address}>
@@ -48,10 +51,13 @@ export default class Latest extends React.Component {
                 ))}
 
                 <br />
-                <h1>Latest assets:</h1>
+                <h2>Asset events:</h2>
+                {/* <h2>Assets events:</h2> */}
+                {/* <h1>Latest assets:</h1> */}
                 {/* <h1>Assets</h1> */}
 
-                <h2>Description added NFT [no destroy]</h2>
+                <h3>Description added NFT [no destroy]</h3>
+                {/* <h2>Description added NFT [no destroy]</h2> */}
                 {/* <h2>Description updated NFT [no destroy]</h2> */}
                 {/* <h2>[RAREST] Latest Updated Locked NFTND</h2> */}
                 {this.state.latest_locked_nftnd.length === 0 ? 'loading...' : null}
@@ -62,7 +68,8 @@ export default class Latest extends React.Component {
                 {this.state.latest_locked_nft_satoshi.map((asset) => formattedAssetElement(asset, asset.latest_description_issuance, IssuanceEvent.TYPES.UPDATE_DESCRIPTION, true))} */}
 
                 {/* <h2>Quantity updated NFT [by destroy]</h2> */}
-                <h2>Quantity changed NFT [by destroy]</h2>
+                <h3>Quantity changed NFT [by destroy]</h3>
+                {/* <h2>Quantity changed NFT [by destroy]</h2> */}
                 {/* <h2>Latest Updated Locked NFT</h2> */}
                 {this.state.latest_locked_nft.length === 0 ? 'loading...' : null}
                 {this.state.latest_locked_nft.map((asset) => formattedAssetInListElement(asset, asset.block_event, IssuanceEvent.TYPES.UPDATE_QUANTITY, true))}
@@ -70,14 +77,16 @@ export default class Latest extends React.Component {
                 {/* {this.state.latest_locked_nft.map((asset) => formattedAssetElement(asset, asset.latest_description_issuance, IssuanceEvent.TYPES.UPDATE_QUANTITY, true))} */}
                 {/* {this.state.latest_locked_nft.map((asset) => formattedAssetElement(asset, true))} */}
 
-                <h2>Latest Locked</h2>
+                <h3>Latest Locked</h3>
+                {/* <h2>Latest Locked</h2> */}
                 {/* <h2>Locked</h2> */}
                 {this.state.latest_locked.length === 0 ? 'loading...' : null}
                 {this.state.latest_locked.map((asset) => formattedAssetInListElement(asset, asset.lock_issuance, IssuanceEvent.TYPES.LOCK))}
                 {/* {this.state.latest_locked.map((asset) => formattedAssetElement(asset, asset.lock_issuance, IssuanceEvent.TYPES.LOCK))} */}
                 {/* {this.state.latest_locked.map((asset) => formattedAssetElement(asset))} */}
 
-                <h2>Created (includes unlocked)</h2>
+                <h3>Created (includes unlocked)</h3>
+                {/* <h2>Created (includes unlocked)</h2> */}
                 {/* <h2>Latest</h2> */}
                 {this.state.latest.length === 0 ? 'loading...' : null}
                 {this.state.latest.map((asset) => formattedAssetInListElement(asset, asset.first_issuance, IssuanceEvent.TYPES.GENESIS))}
