17fc93f238dc9991cbf761ecca1983f259a07199
Juan <dev@juanaleman.com>
2022-09-07 15:58:10 -0400

    adding agreeing to terms to use the application

--- src/App.js
+++ src/App.js
@@ -15,12 +15,14 @@ class App extends React.Component {
 
     this.state = {
       search: '',
+      terms_accepted: false,
     };
 
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
     // this.handleChange = this.handleChange.bind(this);
     // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAgree = this.handleAgree.bind(this);
   }
 
   // TODO? a better lifecycle method?
@@ -44,6 +46,10 @@ class App extends React.Component {
     this.props.router.navigate(`/${to_navigate}`);
     // this.props.router.navigate(`/${this.state.search}`);
     // this.setState({ search: '' });
+  }
+
+  handleAgree(event) {
+    this.setState({ terms_accepted: true });
   }
 
   render() {
@@ -84,8 +90,61 @@ class App extends React.Component {
     //   with_title = `${this.props.router.location.pathname}`;
     // }
 
+    let show_terms = null;
+    if (!this.state.terms_accepted) {
+      show_terms = (
+        <dialog open style={{
+
+          // https://stackoverflow.com/a/43370443
+
+          position: "fixed",
+          // "display": "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          // "justify-content": "center",
+          // "align-items": "center",
+          // padding: "10%",
+
+          // position: "fixed",
+          // // display: "flex",
+          // height: "50%",
+          // width: "50%",
+
+          // "align-items": "center",
+          // "justify-content": "center",
+          // top: "200px !important",
+        }}>
+
+          <h3>bitSTART Terms of Use:</h3>
+
+          <p>
+            Bitcoin is the source of all data in this application.{' '}
+            As a universally open and permissionless network, it can contain ANY kind of content.{' '}
+            {/* Please be aware that bitSTART does not have any control of the contents that are going to be presented. */}
+            bitSTART reads this data and shows it as is, only using metadata (not the content of the data) to determine what to show.
+          </p>
+          <p>
+            <b>
+              If you are using this application, you are agreeing to not make bitSTART responsible for the content that is presented.
+              {/* If you are using this application, you are agreeing to not make bitSTART responsible for anything in relation to the content that is presented. */}
+            </b>
+            {/* You must agree to not make bitSTART responsible for anything in relation to the content that will be presented. */}
+          </p>
+
+          {/* <p>Greetings, one and all!</p> */}
+          {/* <form onSubmit={this.handleAgree} method="dialog"> */}
+          <button onClick={this.handleAgree}>Agree and proceed</button>
+          {' '}
+          {/* <button onClick={this.handleNotAgreeSubmit}>Do not agree, exit the application</button> */}
+          {/* </form> */}
+        </dialog>
+      );
+    }
+
     return (
       <div style={{ padding: "1rem" }}>
+
+        {show_terms}
 
         <h1>bitST.ART{with_title}</h1>
         {/* <h1>bitST.ART{`${this.props.router.location.pathname}`}</h1> */}
