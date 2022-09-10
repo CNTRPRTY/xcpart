7f820520fc974f6ba4d0742df0868df2eecac316
Juan <dev@juanaleman.com>
2022-09-09 21:39:27 -0400

    tag

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -136,7 +136,7 @@ export default class Home extends React.Component {
 
                 <br />
                 <p>
-                    [bitSTART v2022.3]
+                    [bitSTART v2022.4]
                     {/* [bitSTART v2022] */}
                     {/* [bitSTART v2022a] */}
                     <br />
