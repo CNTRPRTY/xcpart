c5385cc286741ed290f6b92030e1fee24e4a6bb2
Juan <dev@juanaleman.com>
2022-09-16 01:14:42 -0400

    removing pause of data header

--- src/routes/latest.jsx
+++ src/routes/latest.jsx
@@ -39,7 +39,7 @@ export default class Latest extends React.Component {
             <main style={{ padding: "1rem" }}>
                 {/* <main style={{ padding: "1rem 0" }}> */}
 
-                <h1><strong>2022-09-10: Updates to the data have paused, but <Link to="/_all">all</Link> past data is still available.</strong></h1>
+                {/* <h1><strong>2022-09-10: Updates to the data have paused, but <Link to="/_all">all</Link> past data is still available.</strong></h1> */}
 
                 <h1>Latest:</h1>
 
